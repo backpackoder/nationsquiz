1f7f3df4150fc1374dabb6d15f7584cb140713dc
harderth <th.barbiera@outlook.fr>
2023-04-01 12:12:22 +0200

    first quiz prototype working

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -1,42 +1,126 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppProviderProps } from "../../types/main";
 import { AppContext } from "../AppContext";
 
 export function Quiz() {
   const { data }: AppProviderProps = useContext(AppContext);
 
-  const arrayOfCountries: number[] = [];
-  const items = 8;
+  const [score, setScore] = useState(0);
 
+  const [responseData, setResponseData] = useState<any>(data[getRandomCountryResponse()]);
+  console.log("responseData", responseData);
+
+  const [hasResponded, setHasResponded] = useState(false);
+  const [wrongAnswer, setWrongAnswer] = useState(false);
+  const [correctAnswer, setCorrectAnswer] = useState(false);
+
+  const arrayOfIDs: any[] = [];
+  if (data) {
+    data.map((item: any) => {
+      arrayOfIDs.push(item.cca2);
+    });
+
+    const search = arrayOfIDs.findIndex((item: any) => item === responseData?.cca2);
+    if (search !== -1) {
+      arrayOfIDs.splice(search, 1);
+    }
+  }
+
+  function getRandomCountryResponse() {
+    return Math.floor(Math.random() * data.length);
+  }
   function getRandomCountry() {
-    const randomCountry = Math.floor(Math.random() * data.length - 1);
+    const randomCountry = Math.floor(Math.random() * arrayOfIDs.length);
+    const countryChosen = arrayOfIDs[randomCountry];
+    const findCountryIndex = data.findIndex((item: any) => item.cca2 === countryChosen);
 
-    if (arrayOfCountries.includes(randomCountry)) {
-      console.log("error!!!");
-      getRandomCountry();
+    arrayOfIDs.splice(randomCountry, 1);
+
+    return findCountryIndex;
+  }
+
+  const items = 4;
+
+  const randomResponseLocation = Math.floor(Math.random() * items);
+  const [responseLocation, setResponseLocation] = useState(randomResponseLocation);
+
+  const otherResponsesLocationArray: any[] = [];
+  for (let i = 0; i < items; i++) {
+    otherResponsesLocationArray.push(getRandomCountry());
+  }
+  const [otherResponsesLocation, setOtherResponsesLocation] = useState<number[]>(
+    otherResponsesLocationArray
+  );
+  console.log("otherResponsesLocation", otherResponsesLocation);
+
+  function resetResponse() {
+    setResponseData(data[getRandomCountryResponse()]);
+    setResponseLocation(randomResponseLocation);
+    setOtherResponsesLocation(otherResponsesLocationArray);
+    setHasResponded(false);
+    setWrongAnswer(false);
+    setCorrectAnswer(false);
+  }
+
+  function choose(index: any) {
+    setHasResponded(true);
+    const isCorrect = index === responseLocation;
+
+    if (isCorrect) {
+      !hasResponded && setCorrectAnswer(true);
+      setScore(score + 1);
     } else {
-      arrayOfCountries.push(randomCountry);
+      !hasResponded && setWrongAnswer(true);
+      setScore(score - 1);
     }
 
-    console.log("arrayOfCountries", arrayOfCountries);
-
-    return randomCountry;
+    setTimeout(
+      () => {
+        resetResponse();
+      },
+      isCorrect ? 1000 : 2000
+    );
   }
 
   return (
-    <div>
-      <h2>Quiz principal</h2>
-      <p>{data[7].name.common}</p>
-      <ul>
-        {Array(items)
-          .fill(0)
-          .map((_, index) => (
-            <li key={index} onClick={() => console.log("item", index)}>
-              {data[getRandomCountry()]?.name?.common}
-            </li>
-          ))}
-      </ul>
-      <button onClick={() => console.log("getRandomCountry", getRandomCountry())}>ici</button>
-    </div>
+    data && (
+      <div className="quiz">
+        <h2>Quiz principal</h2>
+        <p>score: {score}</p>
+        <p>De quel pays est ce drapeau ?</p>
+        <img src={responseData?.flags?.png} alt={responseData?.flags?.alt} />
+
+        <ul>
+          {Array(items)
+            .fill(0)
+            .map((_, index) => {
+              return index === responseLocation ? (
+                <li
+                  key={index}
+                  style={{
+                    backgroundColor:
+                      hasResponded && (wrongAnswer || correctAnswer) ? "lightgreen" : "white",
+                  }}
+                  onClick={() => {
+                    choose(index);
+                  }}
+                >
+                  RESPONSE: {responseData?.name?.common}
+                </li>
+              ) : (
+                <li
+                  key={index}
+                  style={{ backgroundColor: hasResponded && wrongAnswer ? "red" : "white" }}
+                  onClick={() => {
+                    choose(index);
+                  }}
+                >
+                  {data[otherResponsesLocation[index]]?.name?.common}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+    )
   );
 }
